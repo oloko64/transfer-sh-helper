322229342362cf21bf82e37e8ef4d3d60e1c0deb
OLoKo64 <naldoepingo1@gmail.com>
2022-02-15 08:29:48 -0300

    feat: code improvements and code comments

--- tranferShStore.py
+++ tranferShStore.py
@@ -3,39 +3,48 @@ import sqlite3
 from datetime import datetime
 import sys
 
+# Initiate sqlite3 database
 conn = sqlite3.connect('database/transferSh.db')
 c = conn.cursor()
 
+# One week in unix time equals 1209600
+unixWeek = 1209600
 
+# Gets the current unix time
 def currentTime():
     return int(time.time())
 
+# Convert unix time into readable time
 def readableTime(time):
     return datetime.utcfromtimestamp(time).strftime('%d-%m-%Y')
 
+# Check if the provided unix time is a week or more older
 def isOutOfDate(previousDate):
-    # One week in unix time equals 1209600
-    return (currentTime() - previousDate) > 1209600
+    return (currentTime() - previousDate) > unixWeek
 
+# Get all the data from the database
 def readData():
     c.execute("SELECT * from transferData")
 
     data = c.fetchall()
     return data
 
+# Insert provided data into database
 def dataEntry(data):
     c.execute(f"INSERT INTO transferData(name, link, deleteLink, unixTime) VALUES(?, ?, ?, ?)", (data['name'], data['link'], data['deleteLink'], data['unixTime']))
-
+    
     conn.commit()
     c.close()
     conn.close()
 
+# Prints the data to the console
 def printData():
     print()
-    for row in enumerate(readData()):
-        print(f'Id: {list(row[1])[0]} - Name: {list(row[1])[1]} | Link: {list(row[1])[2]} | Delete Link: {list(row[1])[3]} | Created Time: {readableTime(list(row[1])[4])} | Expired Time: {readableTime(list(row[1])[4] + 1209600)} | Expired: {isOutOfDate(list(row[1])[4])}')
+    for row in readData():
+        print(f'Id: {list(row[1])[0]} - Name: {list(row[1])[1]} | Link: {list(row[1])[2]} | Delete Link: {list(row[1])[3]} | Created Time: {readableTime(list(row[1])[4])} | Expired Time: {readableTime(list(row[1])[4] + unixWeek)} | Expired: {isOutOfDate(list(row[1])[4])}')
     print()
 
+# Delete data from the table based on the id provided
 def executeDelete(id):
     c.execute("DELETE from transferData WHERE id = ?", id)
 
@@ -43,15 +52,17 @@ def executeDelete(id):
     c.close()
     conn.close()
 
+# Asks the user what is the id to execute the deletion from the database
 def deleteData():
     printData()
     id = input('Type the id of the entry you want to delete: ')
     executeDelete(id)
 
+# Asks the user for the information to be added to the database
 def insertData():
-    name = input('Type the name of the entry: ')
-    link = input('Type the link of the entry: ')
-    deleteLink = input('Type the delete link of the entry: ')
+    name = input('(1/3) - Type the name of the entry: ')
+    link = input('(2/3) - Type the link of the entry: ')
+    deleteLink = input('(3/3) - Type the delete link of the entry: ')
 
     localData = {
         "name": name,
@@ -62,6 +73,7 @@ def insertData():
 
     dataEntry(localData)
 
+# Prints all the help commands
 def printHelp():
     print('List of commands:')
     print(' -h | --help                => Help on how to use it')
@@ -70,6 +82,7 @@ def printHelp():
     print(' -d | --delete              => Delete data from database')
     print()
 
+# Manage all the arguments provided
 def argParser(args):
     if len(args):
         for arg in args:
@@ -88,6 +101,6 @@ def argParser(args):
         printData()
 
 
-
+# Main code execution
 if __name__ == "__main__":
     argParser(sys.argv[1:])
